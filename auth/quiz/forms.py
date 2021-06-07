9bd8e265dfe38e9fb8e5c5641741fefff1476012
FaridunSalimzoda <61520728+FaridunSalimzoda@users.noreply.github.com>
2021-06-08 02:03:42 +0300

    1. main/views.py --> setting_user 2. Не понятно как сделать 3. Quiz forms.py

--- auth/quiz/forms.py
+++ auth/quiz/forms.py
@@ -20,7 +20,7 @@ class QuizForm(ModelForm):
     class Meta:
         model = Quiz
 
-        fields = ['title', 'description', 'category', 'figure', 'random_order', 'max_questions', 'answers_at_end',
+        fields = ['title', 'description', 'category', 'random_order', 'max_questions', 'answers_at_end',
                   'exam_paper', 'pass_mark', 'single_attempt', 'fail_text', 'draft', 'success_text', 'url']
         widgets = {
             'title': TextInput(attrs={
@@ -34,9 +34,8 @@ class QuizForm(ModelForm):
                 'class': 'form_class',
 
             }),
-            'figure': ImageField(attrs={ #TODO И тут тоже не понятно
 
-            }),
+
             'random_order': CheckboxInput(attrs={
 
             }),
--- auth/users/models.py
+++ auth/users/models.py
@@ -42,7 +42,7 @@ class user(AbstractBaseUser, PermissionsMixin):
     last_name = models.TextField('Фамилия',max_length=255, default=False)
     patronymic = models.TextField('Отчество',max_length=255, default=False, blank=True)
     students_groups = models.TextField('Группа',max_length=10, default='group')
-    photo = models.ImageField('Фото',default='path/to/my/default/image.jpg')
+    photo = models.ImageField(default="")
 
     class Meta:
             verbose_name = 'Пользователя'
