5344a7ab2aa5a0810f1e329b76275d681c7fd203
Vic Istomin <vic.istomin@gmail.com>
2021-06-10 12:54:08 +0300

    Убрал ошибку при запросе результатов в админке

--- auth/quiz/models.py
+++ auth/quiz/models.py
@@ -285,7 +285,7 @@ class Progress(models.Model):
         return Sitting.objects.filter(user=self.user, complete=True)
 
     def __str__(self):
-        return self.user.username + ' - '  + self.score
+        return self.user.name + ' ' + self.user.last_name + ' - '  + self.score
 
 
 class SittingManager(models.Manager):
